ed77fb8c2e69cb2b12f6486c757f2a6414161935
Daniel Davidov <48236638+dannydav709@users.noreply.github.com>
2024-03-07 14:38:35 -0500

    Add more prime_number tests in test_script.py

--- practice_project/test_script.py
+++ practice_project/test_script.py
@@ -6,9 +6,14 @@ def test_prime_numbers():
     assert is_prime(4) == False
     assert is_prime(3) == True
     assert is_prime(5) == True
+    assert is_prime(17) == True
+    assert is_prime(19) == True
+    
 
 
 def test_non_prime_numbers():
     assert is_prime(0) == False
     assert is_prime(1) == False
     assert is_prime(4) == False
+    assert is_prime(9) == False
+    assert is_prime(16) == False
