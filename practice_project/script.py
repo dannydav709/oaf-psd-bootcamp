22b2a8d714c8692666fa163c93aad9166f8ba764
Daniel Davidov <48236638+dannydav709@users.noreply.github.com>
2024-03-07 14:32:59 -0500

    Remove unused code and tests

--- practice_project/script.py
+++ practice_project/script.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 def is_prime(number):
     if number <= 1:
         return False
@@ -14,31 +11,3 @@ def is_prime(number):
             return False
         i += 6
     return True
-
-class TestIsPrime(unittest.TestCase):
-    def test_prime_numbers(self):
-        self.assertTrue(is_prime(2))
-        self.assertTrue(is_prime(3))
-        self.assertTrue(is_prime(5))
-        self.assertTrue(is_prime(7))
-        self.assertTrue(is_prime(11))
-        self.assertTrue(is_prime(13))
-        self.assertTrue(is_prime(17))
-        self.assertTrue(is_prime(19))
-        self.assertTrue(is_prime(23))
-        self.assertTrue(is_prime(29))
-
-    def test_non_prime_numbers(self):
-        self.assertFalse(is_prime(0))
-        self.assertFalse(is_prime(1))
-        self.assertFalse(is_prime(4))
-        self.assertFalse(is_prime(6))
-        self.assertFalse(is_prime(8))
-        self.assertFalse(is_prime(9))
-        self.assertFalse(is_prime(10))
-        self.assertFalse(is_prime(12))
-        self.assertFalse(is_prime(15))
-        self.assertFalse(is_prime(25))
-
-if __name__ == "__main__":
-    unittest.main()
--- practice_project/test_script.py
+++ practice_project/test_script.py
@@ -1,4 +1,4 @@
-import pytest
+# import pytest
 from .script import is_prime
 
 
